55ee6cdeb607411df6df64825c17be8f2e6a6520
Marin Nozhchev <murfffi@gmail.com>
2024-04-07 10:33:21 +0300

    Impala partial copy support

--- internal/integrationtest/impala/impala_test.go
+++ internal/integrationtest/impala/impala_test.go
@@ -2,13 +2,16 @@ package monetdb
 
 import (
 	"context"
+	"fmt"
 	"github.com/sclgo/usqlgen/internal/gen"
 	"github.com/sclgo/usqlgen/internal/integrationtest"
 	"github.com/sclgo/usqlgen/pkg/fi"
+	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/wait"
 	"net"
 	"net/url"
+	"os"
 	"testing"
 )
 
@@ -26,14 +29,67 @@ func TestImpala(t *testing.T) {
 		inp := gen.Input{
 			Imports: []string{"github.com/kprotoss/go-impala"},
 		}
-		integrationtest.CheckGenAll(t, inp, "impala:"+dsn, "select 'Hello World'")
+		t.Run("select", func(t *testing.T) {
+			integrationtest.CheckGenAll(t, inp, "impala:"+dsn, "select 'Hello World'")
+		})
+
+	})
+
+	t.Run("sclgo driver", func(t *testing.T) {
+		inp := gen.Input{
+			Imports:  []string{"github.com/bippio/go-impala"},
+			Replaces: []string{"github.com/bippio/go-impala=github.com/sclgo/go-impala@master"},
+		}
+
+		t.Run("select", func(t *testing.T) {
+			integrationtest.CheckGenAll(t, inp, "impala:"+dsn, "select 'Hello World'")
+		})
+
+		t.Run("copy", func(t *testing.T) {
+			tmpDir := integrationtest.MakeTempDir(t)
+			defer fi.NoErrorF(fi.Bind(os.RemoveAll, tmpDir), t)
+			inp.WorkingDir = tmpDir
+
+			err := inp.All()
+			require.NoError(t, err)
+
+			tableDdl := "create table default.dest(col1 string, col2 string) STORED AS PARQUET;"
+
+			//impala.DefaultOptions.LogOut = os.Stdout
+			//db, err := sql.Open("impala", dsn)
+			//require.NoError(t, err)
+			//defer sclerr.CloseQuietly(db)
+			//_, err = db.Exec(tableDdl)
+			//require.NoError(t, err)
+			//_, err = db.Exec("insert into default.dest values ('1', '2')")
+			//require.NoError(t, err)
+
+			output := integrationtest.RunGeneratedUsql(t, "impala:"+dsn, tableDdl, tmpDir)
+			require.Contains(t, output, "CREATE TABLE")
+
+			destExpression := "INSERT INTO dest VALUES (?, ?)"
+			copyCmd := fmt.Sprintf(`\copy csvq:. impala:%s 'select string(1), string(2)' '%s'`, dsn, destExpression)
+			output = integrationtest.RunGeneratedUsql(t, "", copyCmd, tmpDir)
+			require.Contains(t, output, "COPY")
+
+			output = integrationtest.RunGeneratedUsql(t, "impala:"+dsn, "select * from dest", tmpDir)
+
+			//INSERT does not work on the combination current version of the driver and
+			//the docker environment. Writing to tables backed by local files requires passing the user
+			//which doesn't seem to happen when useLdap = false.
+			//require.Contains(t, output, "(1 row)")
+			require.Contains(t, output, "(0 rows)")
+		})
 	})
 
 	t.Run("kenshaw driver", func(t *testing.T) {
 		inp := gen.Input{
-			Replaces: []string{"github.com/bippio/go-impala=github.com/kenshaw/go-impala@latest"},
+			Replaces: []string{"github.com/bippio/go-impala=github.com/kenshaw/go-impala@master"},
 		}
-		integrationtest.CheckGenAll(t, inp, dsn, "select 'Hello World'", "impala")
+
+		t.Run("select", func(t *testing.T) {
+			integrationtest.CheckGenAll(t, inp, dsn, "select 'Hello World'", "impala")
+		})
 	})
 
 }
@@ -44,6 +100,7 @@ func GetDsn(ctx context.Context, t *testing.T, c testcontainers.Container) strin
 	u := &url.URL{
 		Scheme: "impala",
 		Host:   net.JoinHostPort(host, port),
+		User:   url.User("impala"),
 	}
 	t.Log("url", u.String())
 	return u.String()
@@ -54,7 +111,7 @@ func Setup(ctx context.Context) (testcontainers.Container, error) {
 		Image:        "apache/kudu:impala-latest",
 		ExposedPorts: []string{dbPort},
 		Cmd:          []string{"impala"},
-		WaitingFor:   wait.ForLog("Starting statestore subscriber service"),
+		WaitingFor:   wait.ForLog("Impala has started."),
 	}
 	return testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
 		ContainerRequest: req,
