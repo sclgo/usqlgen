40f2ec4aea1c15b33327e78a806cf1d97c7609ac
Marin Nozhchev <murfffi@gmail.com>
2024-12-01 08:42:30 +0200

    extended test

--- internal/shell/cmd_build_test.go
+++ internal/shell/cmd_build_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/sclgo/usqlgen/internal/run"
 	"github.com/stretchr/testify/require"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"testing"
 )
@@ -18,18 +19,12 @@ func main() {
 `
 
 func TestBuild(t *testing.T) {
-	t.Run("stdout", func(t *testing.T) {
+	t.Run("build to stdout", func(t *testing.T) {
 		cmd := BuildCommand{
 			CompileCommand: CompileCommand{
 				CommandBase: Base(&GlobalParams{}),
-				generator: func(input gen.Input) error {
-					err := run.Go(input.WorkingDir, "mod", "init", "a")
-					if err != nil {
-						return err
-					}
-					return os.WriteFile(filepath.Join(input.WorkingDir, "main.go"), []byte(main), 0644)
-				},
-				goBin: run.FindGo(),
+				generator:   minimalGoGenerator,
+				goBin:       run.FindGo(),
 			},
 			output: "-",
 		}
@@ -38,5 +33,21 @@ func TestBuild(t *testing.T) {
 		err := cmd.Action(&buf)
 		require.NoError(t, err)
 		require.NotEmpty(t, buf)
+
+		outputTmpFile := filepath.Join(t.TempDir(), "usql")
+		err = os.WriteFile(outputTmpFile, buf.Bytes(), 0644)
+		require.NoError(t, err)
+		result, err := exec.Command("file", outputTmpFile).Output()
+		require.NoError(t, err)
+		require.Contains(t, string(result), "LSB executable")
+		require.Contains(t, string(result), "ELF")
 	})
+}
+
+func minimalGoGenerator(input gen.Input) error {
+	err := run.Go(input.WorkingDir, "mod", "init", "a")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(filepath.Join(input.WorkingDir, "main.go"), []byte(main), 0644)
 }
