sclgo/usqlgen

commit a843cffcdbbbc864de9df657d57801d5ff83a63e

feat: avoid downloading unnecessary dependencies (#48)
Avoid running `go mod tidy` when building with `--import` only. `go mod
tidy` pulls transitive test dependencies which are not needed just to
build usql -
https://go.dev/wiki/Modules#why-does-go-mod-tidy-record-indirect-and-test-dependencies-in-my-gomod

The change speeds up significantly the `build` and `install` commands
reduces the amount of bandwidth used. For example, for `--import
github.com/sclgo/impala-go` this approach leads to 521 MB of Go mod
cache, measured on a clean `golang:1.24` container with

```shell
du -sh $(go env GOMODCACHE)
```

as opposed to 3.1 GB with latest release - **6x improvement**.

Time improvement on my machine is 44 sec on this branch vs. 56 sec on
latest. The time improvement varies though based on the bandwidth. There
is little improvement in pipeline time because actions/setup-go caches
modules across runs. Still the Post step of setup-go is sec faster in
part because the cache is smaller - 2.1 GB vs 2.6 GB.

The PR also upgrades actions/setup-go from 4 to 5.3.0 with huge
improvement in time spent there - 3 min less. actions/checkout is also
updated.

Finally, the PR removes the `go mod edit -go=1.23` command which is not
needed with recent versions of `usql` . It is left commented out in case
someone wants to use an old usql version.

internal/gen/gen.go
@@ -28,7 +28,12 @@ var dbMgrCode []byte
 const fileMode = 0700
 
 type Input struct {
-	Imports     []string
+
+	// Imports lists packages that will be imported with blank import
+	// in the order given. Duplicates are allowed and won't be removed.
+	// Caller should remove duplicates if needed.
+	Imports []string
+
 	Replaces    []string
 	Gets        []string
 	WorkingDir  string
@@ -85,11 +90,11 @@ func (i Input) AllDownload() (Result, error) {
 		result.DownloadedUsqlVersion = fmt.Sprint(downloadedVersion)
 	}
 
-	// TODO Check if already is 1.23+
-	err = i.runGo("mod", "edit", "-go=1.23")
-	if err != nil {
-		return result, merry.Wrap(err)
-	}
+	// We expect that the DownloadedUsqlVersion is already at go1.23+
+	//err = i.runGo("mod", "edit", "-go=1.23")
+	//if err != nil {
+	//	return result, merry.Wrap(err)
+	//}
 
 	origMainFile := filepath.Join(i.WorkingDir, "main.go")
 	origMainBytes, err := os.ReadFile(origMainFile)
@@ -120,10 +125,6 @@ func (i Input) AllDownload() (Result, error) {
 	err = i.goModReplace(i.Replaces)
 	if err != nil {
 		return result, err
-	}
-
-	if len(i.Replaces) == 0 {
-		err = i.runGo("mod", "tidy")
 	}
 
 	return result, err
@@ -289,7 +290,26 @@ func (i Input) populateDbMgr() error {
 }
 
 func (i Input) doGoGet() error {
-	for _, gs := range i.Gets {
+	return doGoGet(i.Gets, i)
+}
+
+// We believe that we don't need to go get the --imports params,
+// since this is handled by CompileCmd using either -mod=mod for build/install,
+// or "go mod tidy for generate
+//
+//func (i Input) getImports() error {
+//	if i.Gets != nil {
+//		// The modules in get may have already delivered those packages
+//		// We don't want to run 'go get' for them because it will upgrade the explicit version
+//		i.Imports = lo.Filter(i.Imports, func(item string, index int) bool {
+//			return i.runGo("list", "-find", item) != nil // returns exit code 1 if not found
+//		})
+//	}
+//	return doGoGet(i.Imports, i)
+//}
+
+func doGoGet(gets []string, i Input) error {
+	for _, gs := range gets {
 		err := i.runGo("get", gs)
 		if err != nil {
 			return err

internal/shell/cmd_compile.go
@@ -1,7 +1,6 @@
 package shell
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 
@@ -43,17 +42,27 @@ func (c *CompileCommand) compile(compileCmd string, compileArgs ...string) error
 	}
 
 	if compileCmd == "" {
-		return nil
+		return run.GoBin(workingDir, c.goBin, "mod", "tidy")
 	}
 
 	args := []string{compileCmd}
 	args = append(args, compileArgs...)
 	// -ldflags can be repeated so this doesn't interfere with PassthroughArgs
-	ldflags := fmt.Sprintf(`-X github.com/xo/usql/text.CommandVersion=%s-usqlgen`, genResult.DownloadedUsqlVersion)
+	ldflags := `-X github.com/xo/usql/text.CommandVersion=` + makeVersion(genResult.DownloadedUsqlVersion)
 	args = append(args, "-ldflags", ldflags)
+
+	// NB: This might interfere with PassthroughArgs
+	// Required to avoid go mod tidy when adding just imports
+	args = append(args, "-mod=mod")
+
 	args = append(args, c.Globals.PassthroughArgs...)
 	args = append(args, ".")
 	return run.GoBin(workingDir, c.goBin, args...)
+}
+
+func makeVersion(downloadedVersion string) string {
+	// we use _ as separator so it doesn't interfere with the suggested go install logic in usql/main.go
+	return downloadedVersion + "_usqlgen"
 }
 
 func (c *CompileCommand) generate(workingDir string) (gen.Result, error) {

.github/workflows/go.yml
@@ -15,10 +15,10 @@ jobs:
     # ubuntu allows for docker support - https://docs.github.com/en/actions/using-containerized-services/about-service-containers
     runs-on: ubuntu-latest
     steps:
-    - uses: actions/checkout@v3
+    - uses: actions/checkout@v4.2.2
 
     - name: Set up Go
-      uses: actions/setup-go@v4
+      uses: actions/setup-go@v5.3.0
       with:
         go-version: '1.22'
 

internal/gen/gen_test.go
@@ -73,7 +73,7 @@ func runGenAll(t *testing.T, inp gen.Input) string {
 	err := inp.All()
 	require.NoError(t, err)
 
-	cmd := exec.Command("go", "run", ".", "-c", `\drivers`)
+	cmd := exec.Command("go", "run", "-mod=mod", ".", "-c", `\drivers`)
 	cmd.Dir = tmpDir
 	var buf bytes.Buffer
 	cmd.Stdout = io.MultiWriter(&buf, os.Stdout)

internal/integrationtest/build_test.go
@@ -44,5 +44,5 @@ func TestVersion(t *testing.T) {
 	cmd.Stderr = os.Stderr
 	require.NoError(t, cmd.Run())
 
-	require.Equal(t, fmt.Sprintf("usql %s-usqlgen", testVersion), strings.TrimSpace(buf.String()))
+	require.Equal(t, fmt.Sprintf("usql %s_usqlgen", testVersion), strings.TrimSpace(buf.String()))
 }

internal/integrationtest/common.go
@@ -57,7 +57,7 @@ func RunGeneratedUsqlE(dsn string, command string, tmpDir string, tags ...string
 		tags = []string{NoBaseTag}
 	}
 
-	cmd := exec.Command("go", "run", "-tags", strings.Join(tags, ","), ".", dsn, "-c", command)
+	cmd := exec.Command("go", "run", "-mod=mod", "-tags", strings.Join(tags, ","), ".", dsn, "-c", command)
 	cmd.Dir = tmpDir
 	var buf bytes.Buffer
 	cmd.Stdout = io.MultiWriter(&buf, os.Stdout)

internal/integrationtest/cosmos/cosmos_test.go
@@ -28,7 +28,7 @@ func TestCosmos(t *testing.T) {
 	err := inp.All()
 	require.NoError(t, err)
 
-	cmd := exec.Command("go", "run", "-tags", integrationtest.NoBaseTag, ".", "gocosmos:AccountEndpoint=https://localhost;AccountKey=test", "-c", `LIST DATABASES;`)
+	cmd := exec.Command("go", "run", "-mod=mod", "-tags", integrationtest.NoBaseTag, ".", "gocosmos:AccountEndpoint=https://localhost;AccountKey=test", "-c", `LIST DATABASES;`)
 	cmd.Dir = tmpDir
 	var buf bytes.Buffer
 	cmd.Stdout = io.Discard

internal/shell/cmd_build_test.go
@@ -70,7 +70,23 @@ func TestBuild(t *testing.T) {
 		var buf bytes.Buffer
 		err := cmd.Action(&buf)
 		require.NoError(t, err)
-		require.Contains(t, buf.String(), testVersion+"-usqlgen")
+		require.Contains(t, buf.String(), testVersion+"_usqlgen")
+	})
+
+	t.Run("full build", func(t *testing.T) {
+		fi.SkipLongTest(t)
+
+		cmd := NewCommands(nil).BuildCmd
+		require.NoError(t, cmd.Imports.Set("github.com/sclgo/impala-go"))
+
+		currentWorkDir := fi.NoError(os.Getwd()).Require(t)
+		defer fi.NoErrorF(fi.Bind(os.Chdir, currentWorkDir), t)
+		require.NoError(t, os.Chdir(tmpDir))
+		err := cmd.Action(nil)
+		require.NoError(t, err)
+
+		outputTmpFile := filepath.Join(tmpDir, "usql")
+		checkExecutable(t, outputTmpFile)
 	})
 }
 

commit 4300ba7e0449dabeb4aaf9c3b2a0b9dedb19fe08

fix: ensure toolchain is upgraded (#77)
The optimization added in #48 no longer supports properly upgrading the
Go toolchain if a new
dependency requires it. It is unclear if the regression existed ever
since #48 or the behavior of the `-mod=mod`
parameter changed since the optimization was introduced.

This PR reverts to the old behavior of running `go mod tidy` after the
generation instead of adding `-mod=mod` to `go build` unfortunately
erasing performance benefits of #48. In a future PR, the optimization
may be reintroduced as an optional generation parameter because it is
still valuable if toolchain upgrades are not needed.

internal/gen/gen.go
@@ -113,8 +113,7 @@ func (i Input) AllDownload() (Result, error) {
 	}
 
 	// We believe that we don't need to "go get" packages in the --imports params,
-	// since this is handled by CompileCmd using either -mod=mod for build/install,
-	// or "go mod tidy for generate
+	// because we run go mod tidy at the end.
 
 	err = i.populateDbMgr()
 	if err != nil {
@@ -137,6 +136,11 @@ func (i Input) AllDownload() (Result, error) {
 			i.log("Failed to adjust base cgo tags, but this might not be an issue, depending on tags and environment. Cause: %v", adjustErr)
 		}
 	}
+
+	// goModReplace may have executed go mod tidy but executing again is fast, and we don't need to make
+	// the code more complex to remove the additional call
+	// Note that go build -mod=mod doesn't work if one of the dependencies needs the toolchain to be updated.
+	err = i.runGo("mod", "tidy")
 
 	return result, err
 }

internal/shell/cmd_compile.go
@@ -47,7 +47,7 @@ func (c *CompileCommand) compile(compileCmd string, compileArgs ...string) error
 	}
 
 	if compileCmd == "" {
-		return run.GoBin(workingDir, nil, c.goBin, "mod", "tidy")
+		panic("compileCmd is empty")
 	}
 
 	var addEnv []string
@@ -67,9 +67,10 @@ func (c *CompileCommand) compile(compileCmd string, compileArgs ...string) error
 		args = append(args, "-trimpath")
 	}
 
-	// NB: This might interfere with PassthroughArgs
-	// Required to avoid go mod tidy when adding just imports
-	args = append(args, "-mod=mod")
+	// -mod=mod used to be added to the build command to avoid running go mod tidy in gen.go
+	// However, -mod=mod does not upgrade the toolchain when needed regardless of the GOTOOLCHAIN setting
+	// TODO find documentation or log bug. Based on existing github issues, support for -mod=mod is spotty in general.
+	// Also it might interfere with PassthroughArgs.
 
 	args = append(args, c.Globals.PassthroughArgs...)
 	args = append(args, ".")